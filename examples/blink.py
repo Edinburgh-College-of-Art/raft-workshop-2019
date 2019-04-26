9e468857c0a75966eb817d84b9a792091523125e
mhamilt <matt.hamilton@ed.ac.uk>
2019-04-26 13:27:54 +0100

    add blink script

--- examples/blink.py
+++ examples/blink.py
@@ -3,6 +3,15 @@ from grove.gpio import GPIO
 
 led = GPIO(12, GPIO.OUT)
 
-while True:
-    led.write(not led.read())
-    time.sleep(0.5)
+print("connect an LED to pin #12\nPress CTRL + c to quit")
+
+try:
+    while True:
+        led.write(not led.read())
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    print("Done!")
+    led.write(False)
+    quit()
+
+
