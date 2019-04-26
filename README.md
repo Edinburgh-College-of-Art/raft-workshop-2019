3537809e312eec73151e6b9bf05bd45159264549
mhamilt <matt.hamilton@ed.ac.uk>
2019-04-26 15:18:39 +0100

    Add: example ripts: ble, osc and lsm303

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # raft-workshop-2019
 Scripts, environments and code snippets for the RAFT Workshop 2019
+
+
+## Suggested Applications
+
+### [Adafruit BLE](https://github.com/adafruit/Adafruit_Python_BluefruitLE)
+
+```bash
+git clone https://github.com/adafruit/Adafruit_Python_BluefruitLE
+cd Adafruit_Python_BluefruitLE
+```
+#### macOS
+
+```bash
+sudo python setup.py install
+sudo python uart_service.py
+```
--- install.sh
+++ install.sh
@@ -1,6 +1,51 @@
+# ------------------------------------------------------------
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install \
+              nodejs \
+                 npm \
+       libcairo2-dev \
+            puredata \
+        librxtx-java \ # for processing
+       openjdk-6-jdk \ # for processing
+# ------------------------------------------------------------
+# Install Processing
+curl https://processing.org/download/install-arm.sh | sudo sh
+
+# ------------------------------------------------------------
+# Bluetooth
+cd ~
+wget http://www.kernel.org/pub/linux/bluetooth/bluez-5.37.tar.xz
+tar -xf bluez-5.37.tar.xz
+cd bluez-5.37
+sudo apt-get install -y \
+             libusb-dev \
+          libdbus-1-dev \
+         libglib2.0-dev \
+            libudev-dev \
+            libical-dev \
+        libreadline-dev
+
+./configure
+make
+sudo make install
+
+systemctl status bluetooth
+# ------------------------------------------------------------
+# Python OSC
+pip3 install python-osc
+git clone https://github.com/toddtreece/osc-examples.git
+cd osc-examples
+npm install
+cd ..
+# ------------------------------------------------------------
+# lsm303
+sudo pip3 install adafruit-lsm303
+# ------------------------------------------------------------
+# Grove Sensors
 git clone https://github.com/Seeed-Studio/grove.py
 cd grove.py
 sudo pip3 install .
 cd ..
-
 python3 examples/blink.py
+# ------------------------------------------------------------
